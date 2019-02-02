117afeb4a295c641ac64d8ec046eac46501c2755
visualstudio <visualstudio@122-PC03>
2019-02-01 20:34:02 -0500

    MANPREETT

--- C0730406 A/Program.cs
+++ C0730406 A/Program.cs
@@ -10,33 +10,68 @@ namespace C0730406_A
     {
         static void Main(string[] args)
         {
-            countryside c = new countryside();
-        }
-
-            class village
-        {
-            bool isAstrildeHere;
-           public  village nextvillage;
-             public village prevvillage;
-           public String villagename;
+            Countryside c = new Countryside();
+            c.run();
 
 
         }
-
-        class countryside
+    }
+    class LearningExperiment
+    {
+        public void run()
         {
             village Toronto;
-            village maple;
-            village ajax;
+            Toronto = new village();
+            village a, b, c;
+            Toronto.villagename = "version A";
+            a = Toronto;
+            Console.WriteLine(a.villagename);
+            Toronto = new village();
+            Toronto.villagename = "version B";
+            b = Toronto;
+            Console.WriteLine(a.villagename);
+            Toronto = new village();
+            Toronto.villagename = "version C";
+            c = Toronto;
+            Console.WriteLine(a.villagename);
+        }
+    }
+    class village
+    {
+        public bool isAstrilde;
+        public village nextvillage;
+        public village prevvillage;
+        public string villagename;
 
-            public void rise()
+    }
+
+    class Countryside
+    {
+        village Maple;
+        village Toronto;
+        village Ajax;
+
+        public void run()
+        {
+            Maple = new village();
+            Toronto = new village();
+            Ajax = new village();
+            Ajax.isAstrilde = true;
+            Maple.villagename = "Toronto";
+            Maple.nextvillage = Toronto;
+        }
+        public void travel()
+        {
+            village currentvillage = Toronto;
+            while (!currentvillage.isAstrilde)
             {
-                maple = new village();
-                maple.villagename =" Toronto";
-                maple.nextvillage = Toronto;
+                if (currentvillage.isAstrilde)
+                    Console.WriteLine("You found Astrilde in " + currentvillage);
+                else
+                    currentvillage = currentvillage.nextvillage;
             }
         }
-
     }
 }
 
+    
